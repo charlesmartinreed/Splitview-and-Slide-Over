951db5bdf80882e2c15041f594bd458518a2c522
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-08-27 12:45:12 -0500

    Initial Commit

---
+++ Project31/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Project31
+//
+//  Created by Charles Martin Reed on 8/27/18.
+//  Copyright © 2018 Charles Martin Reed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var addressBar: UITextField!
+    @IBOutlet weak var stackView: UIStackView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
